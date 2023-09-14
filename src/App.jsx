b7e45a95478c0d8827eb199acb0ea5fe837a3fa3
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-14 10:58:38 +0600

    sign out setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
+import { useState } from 'react';
 import app from '../firebase.init'
 import './App.css'
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 
 let auth = new getAuth(app);
 
 function App() {
+  let [user, setUser] = useState({});
   let provider = new GoogleAuthProvider();
   let signInGogole = () => {
     signInWithPopup(auth, provider)
-    .then(result => console.log(result))
+    .then(result => {
+      let user = result.user;
+      setUser(user)
+      console.log(result)
+    })
     .catch(error => console.log(error));
+  }
+  let signOutAccount = () => {
+    signOut(auth)
+    .then(result => setUser({}))
+    .catch(error => console.log(error))
   }
   return (
       <div>
-        <button onClick={signInGogole}>Sign in</button>
+        {
+          user.uid ? <button onClick={signOutAccount}>Sign out</button> : <button onClick={signInGogole}>Sign in</button>
+        }
+        
+        <h1>Name: {user.displayName}</h1>
+        <img src={user.photoURL} />
       </div>
   )
 }
